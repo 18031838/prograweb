7a92664e84059735a39e578ff94e789bf7c1622a
18031838 <18031838@itcelaya.edu.mx>
2023-09-12 23:40:55 -0600

    Clase12_09_2023
    Ultima correcion de html y primer dia de php

---
+++ controladores/ctrContacto.php
@@ -0,0 +1,15 @@
+<!--
+    No hay manera de purificar este codigo
+    por lo que los devs no es gusta
+    se ocupa un formulario para manejralo, desde el action
+    de los formularios
+
+    modelo vista controlador separar los codigos
+
+    para recuperar los datos del formulario debemos recuperar los
+    id y el nombre de los campos
+-->
+<?php
+    echo 'HOLA '.$_POST['txtNombre'];
+    
+?>
